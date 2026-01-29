81bea97f0fdbd58d8e98cc9526882c0b4fca72b7
François Pitié <francois.pitie@gmail.com>
2026-01-29 13:48:24 +0000

    Fix asset paths for local serving

--- website/vite.config.js
+++ website/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/4c16/',
+  base: './',
   build: {
     outDir: '../docs',
     emptyOutDir: true,
